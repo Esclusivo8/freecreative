2089ce3c2d8aab60b7243ec1a3e1c68a5145658f
Esclusivo8 <marcogaeta13@gmail.com>
2022-03-27 23:44:06 +0200

    final

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $(function () {
+        $("#accordion").accordion();
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        navText: ['', ' '],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
+
+
